7249ce623add7989a329295a274f34e3f3327ec9
bairgran <grantbaird28@gmail.com>
2026-09-02 17:16:32 -0700

    Seperate board methods from game methods

--- board.py
+++ board.py
@@ -1,6 +1,3 @@
-import requests
-import json
-
 class Board:
     """A sudoku game board.
 
@@ -28,40 +25,6 @@ class Board:
         retStr = ''.join(values)
         return f'{retStr}'
 
-    def createFilePayload(self, values: dict, solution: dict) -> dict: # TODO: documentation
-        payload = {
-            "values": values,
-            "solution": solution
-        }
-        return payload
-
-    def dosukuInit(self): # TODO: documentation
-        url_api = "https://sudoku-api.vercel.app/api/dosuku"
-        query = {'query': '{newboard(limit:1){grids{value,solution}}}'}
-        r = requests.get(url_api, params=query)
-        data = r.json()
-        grid = data['newboard']['grids'][0]['value']
-        solut = data['newboard']['grids'][0]['solution']
-        valuesToFile = {}
-        solutionToFile = {}
-        for r in range(len(grid)):
-            for c in range(len(grid[r])):
-                valuesToFile[self.rows[r]+self.cols[c]] = f'{grid[r][c]}'
-                solutionToFile[self.rows[r]+self.cols[c]] = f'{solut[r][c]}'
-        payloadForFile = self.createFilePayload(valuesToFile, solutionToFile) # TODO: use saveBoard to reduce repeat code.
-        with open('scratch.json', 'w') as f:
-            json.dump(payloadForFile, f, indent=4)
-
-    def loadBoard(self): # TODO: documentation
-        with open('scratch.json', 'r') as f:
-            filecontents = json.loads(f.read())
-            self.sudokuBoard = filecontents['values']
-            self.solution = filecontents['solution']
-
-    def saveBoard(self): # TODO: documentation
-        payload = self.createFilePayload(self.sudokuBoard, self.solution)
-        with open('scratch.json', 'w') as f:
-            json.dump(payload, f, indent=4)
 
     def displayboard(self): # TODO: documentation
         header = self.cols.partition('456')
@@ -116,8 +79,3 @@ class Board:
             pass
         else:
             self.sudokuBoard[sqCoord] = '0'
-
-    def checkBoard(self): # TODO: documentation
-        if self.solution == self.sudokuBoard:
-            return True
-        return False
--- game.py
+++ game.py
@@ -1,3 +1,5 @@
+import requests
+import json
 import board
 
 class Game:
@@ -5,4 +7,43 @@ class Game:
     def __init__(self):
         self.gameboard = board.Board()
 
+    def createFilePayload(self, values: dict, solution: dict) -> dict: # TODO: documentation
+        payload = {
+            "values": values,
+            "solution": solution
+        }
+        return payload
     
+    def dosukuInit(self): # TODO: documentation
+        url_api = "https://sudoku-api.vercel.app/api/dosuku"
+        query = {'query': '{newboard(limit:1){grids{value,solution}}}'}
+        r = requests.get(url_api, params=query)
+        data = r.json()
+        grid = data['newboard']['grids'][0]['value']
+        solut = data['newboard']['grids'][0]['solution']
+        valuesToFile = {}
+        solutionToFile = {}
+        for r in range(len(grid)):
+            for c in range(len(grid[r])):
+                valuesToFile[self.rows[r]+self.cols[c]] = f'{grid[r][c]}'
+                solutionToFile[self.rows[r]+self.cols[c]] = f'{solut[r][c]}'
+        payloadForFile = self.createFilePayload(valuesToFile, solutionToFile) # TODO: use saveBoard to reduce repeat code.
+        with open('scratch.json', 'w') as f:
+            json.dump(payloadForFile, f, indent=4)
+
+    def loadBoard(self): # TODO: documentation
+        with open('scratch.json', 'r') as f:
+            filecontents = json.loads(f.read())
+            self.sudokuBoard = filecontents['values']
+            self.solution = filecontents['solution']
+
+    def saveBoard(self): # TODO: documentation
+        payload = self.createFilePayload(self.sudokuBoard, self.solution)
+        with open('scratch.json', 'w') as f:
+            json.dump(payload, f, indent=4)
+
+    
+    def checkBoard(self): # TODO: documentation
+        if self.solution == self.sudokuBoard:
+            return True
+        return False
